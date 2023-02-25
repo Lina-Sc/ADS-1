e3fe6fd8799d11e3e5650973e7edb7831948afd5
Lina-Sc <125125270+Lina-Sc@users.noreply.github.com>
2023-02-25 10:11:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,64 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    int a = 0;
+    for (int i = 1; i <= value; ++i) {
+        if (value % i == 0)
+            a += 1;
+    }
+    if (a == 2)
+        return true;
+    else
+        return false;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    const int SIZE = 100 * 1000;
+    const int N = 9500;
+    int a[SIZE];
+    for (int i = 2; i < SIZE; ++i)
+        a[i] = 1;
+    int k = 1;
+    int value = 2;
+    while (k < n) {
+        for (int i = 2 * p; i < SIZE; i += value)
+            a[i] = 0;
+        for (int i = value + 1; i < SIZE; ++i)
+            if (a[i] == 1) {
+                p = i;
+                break;
+            }
+        ++k;
+    }
+
+    return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    int b = 0;
+    for (int k = 0; k < value; k++) {
+        value += 1;
+        for (int j = 0; j <= value; ++j) {
+            if (value % j == 0)
+                b += 1;
+        }
+        if (b == 2)
+            return value;
+        if (value)
+            k += value;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    int m = 0;
+    int l = 0;
+    for (int c = 2; c < hbound; ++c) {
+        for (int q = 1; q <= c; ++q) {
+            if (c % q == 0)
+                l += 1;
+        }
+        if (l == 2)
+            m += c;
+    }
+    return m;
 }
