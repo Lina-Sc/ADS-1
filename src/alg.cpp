1079bc5af11a542d53c582ee37ef234a2143fd75
Lina-Sc <125125270+Lina-Sc@users.noreply.github.com>
2023-02-26 10:08:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,4 @@
-// Copyright 2022 NNTU-CS 
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
@@ -48,7 +48,7 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
     uint64_t s = 0;
     for (uint64_t c = 2; c <= hbound; ++c) {
-        if (checkPrime(i)) {
+        if (checkPrime(c)) {
             s += c;
         }
     }
